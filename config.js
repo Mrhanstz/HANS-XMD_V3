0bee5fd7ca9baa5c185ef78d3df45b707bcb1a81
bryant2702 <bataillebryant52@icloud.com>
2025-05-03 08:37:16 -0400

    Update config.js
    Bot free

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || '',
+module.exports = { session: process.env.SESSION_ID || 'HansTz&YCs3DIwA#G1BVo8_pKQ1DV2elSU1LpyKkHqlv2SSlyEyRlECEi0o',
     PREFIXE: process.env.PREFIX || ".",
     OWNER_NAME: process.env.OWNER_NAME || "HANS-XMD",
     NUMERO_OWNER : process.env.NUMERO_OWNER || " 263717087382",              
